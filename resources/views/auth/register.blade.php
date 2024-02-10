66a229704cf03ee09169301c39ff83efff43741f
Hhidek2211 <devhhide1011@gmail.com>
2024-02-10 17:36:11 +0900

    add manager setting in regester process

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,7 +4,7 @@
 
         <!-- Name -->
         <div>
-            <x-input-label for="名前" :value="__('Name')" />
+            <x-input-label for="name" :value="__('Name')" />
             <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
         </div>
@@ -37,6 +37,13 @@
                             name="password_confirmation" required autocomplete="new-password" />
 
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+        </div>
+
+        <!-- 管理者情報の登録-->
+        <div class="mt-4">
+            <x-input-label for="ismanager" :value="__('ismanager')" />
+
+            <input name="ismanager" type="checkbox"> 
         </div>
 
         <div class="flex items-center justify-end mt-4">
